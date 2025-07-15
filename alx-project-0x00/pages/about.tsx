2cb59d68835c1d624e1495f339320eecc0a848b8
RUDYMAZE <njockrudiger4@gmail.com>
2025-07-15 19:10:47 +0300

    made use of the Routing provided in Next.js by creating a landing and an about page

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      {/* Add some content here */}
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Landing Page</h1>
+      {/* Add some content here */}
+    </div>
+  );
+};
+
+export default LandingPage;
