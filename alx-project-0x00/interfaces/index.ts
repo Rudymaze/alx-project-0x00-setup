7281a10831423f45ea78b202f11a078db9420f83
RUDYMAZE <njockrudiger4@gmail.com>
2025-07-15 20:22:06 +0300

    created a button component and and also created various path in landing.tsx

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -3,7 +3,7 @@ export interface PillProps {
 }
 export interface ButtonProps {
   title: string;
-  style?: string;
+  styles: string;
   size: "small" | "medium" | "large";
   shape: "rounded-sm" | "rounded-md" | "rounded-lg";
 }
